5365cae21269f1292c2633c023a7d4ad03585596
L4cache <42721712+L4cache@users.noreply.github.com>
2025-10-13 23:09:14 +0800

    add vulkan to Requires.private instead of Libs

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -822,10 +822,11 @@ if [[ $ffmpeg != no ]] && enabled whisper &&
     mv -f "$LOCALDESTDIR"/lib/ggml-cpu.a "$LOCALDESTDIR"/lib/libggml-cpu.a
     mv -f "$LOCALDESTDIR"/lib/ggml-vulkan.a "$LOCALDESTDIR"/lib/libggml-vulkan.a
     if [[ $CC =~ clang ]]; then
-        sed -i "s|-lggml  -lggml-base -lwhisper|-lwhisper -lggml -lggml-vulkan -lvulkan -lcfgmgr32 -lggml-cpu -lggml-base -lomp|" "$LOCALDESTDIR"/lib/pkgconfig/whisper.pc
+        sed -i "s|-lggml  -lggml-base -lwhisper|-lwhisper -lggml -lggml-vulkan -lggml-cpu -lggml-base -lomp|" "$LOCALDESTDIR"/lib/pkgconfig/whisper.pc
     else
-        sed -i "s|-lggml  -lggml-base -lwhisper|-lwhisper -lggml -lggml-vulkan -lvulkan -lcfgmgr32 -lggml-cpu -lggml-base -lgomp|" "$LOCALDESTDIR"/lib/pkgconfig/whisper.pc
+        sed -i "s|-lggml  -lggml-base -lwhisper|-lwhisper -lggml -lggml-vulkan -lggml-cpu -lggml-base -lgomp|" "$LOCALDESTDIR"/lib/pkgconfig/whisper.pc
     fi
+    grep_or_sed "Requires.private:" "$LOCALDESTDIR"/lib/pkgconfig/whisper.pc "s|Libs:|Requires.private: vulkan\nLibs:|"
     do_checkIfExist
     unset extracommands
 fi
